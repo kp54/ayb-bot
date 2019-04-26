cc63142d7cd9f83109ba38ec8716d216226de4e0
kp54 <kangpang65@gmail.com>
2019-04-26 11:20:00 +0900

    ファイル分けなくても良さそう

--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+func loadNotes(filename string) ([]string, error) {
+	var (
+		err   error
+		bytes []byte
+		notes []string
+	)
+
+	if bytes, err = ioutil.ReadFile(filename); err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(bytes, &notes); err != nil {
+		return nil, err
+	}
+
+	return notes, nil
+}
 
 func main() {
 	fmt.Print("Hello, world!")
